fixed signs and mass staying positive

main.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from model import JetPropulsion
 from integration import rk_four
 
-SIM_TIME = 10.0
+SIM_TIME = 15.0
 T = 0.04
 
 t = np.arange(0.0, SIM_TIME, T)
@@ -31,7 +31,7 @@ jetter = JetPropulsion(waterDens, areaEject, areaFront, dragCoeff)
 #Simulate Open Loop System
 
 x_init = np.zeros(2)
-x_init[0] = 5.0
+x_init[0] = 25.0
 x_init[1] = 0.0 
 
 x = np.zeros((2, N))
@@ -43,7 +43,7 @@ for k in range(1,N):
 
     x[:,k] = rk_four(jetter.f, x[:, k-1], u[:, k-1], T)
 
-    u[:,k] = -5 * (np.sin(0.1 * k))
+    u[:,k] = -3 * (np.sin(0.1 * k))
 
 
 # %%
@@ -77,7 +77,7 @@ plt.plot(t, x[0, :], label="x₁")
 plt.plot(t, x[1, :], label="x₂")
 plt.plot(t, u[0, :], "--", label="u")
 plt.xlabel("Time [s]")
-plt.legend()
+plt.legend(loc="upper right")
 plt.grid(True)
 plt.show()
 